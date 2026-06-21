317840102f9512746bf29293b878cb3cdfa863c0
MURAOKA Taro <koron.kaoriya@gmail.com>
2026-06-21 14:33:57 +0900

    Connect to a DuckDB file via HTTP

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-(async (g)=> {
-  "use strict"
+import * as duckdb from 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.33.1-dev56.0/+esm';
 
-  const SQL = await initSqlJs({
-    locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.13.0/${file}`
-  });
+const bundle = await duckdb.selectBundle(duckdb.getJsDelivrBundles());
+const worker_url = URL.createObjectURL(new Blob([`importScripts("${bundle.mainWorker}");`], {type: 'text/javascript'}));
 
-  const resp = await fetch('/photospa.db');
+// Create a worker and instantiate the database
+const worker = new Worker(worker_url);
+const db = new duckdb.AsyncDuckDB(new duckdb.ConsoleLogger(), worker);
+await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+// Revoke the worker URL.
+URL.revokeObjectURL(worker_url);
 
-  const data = new Uint8Array(await resp.arrayBuffer());
 
-  const db = new SQL.Database(data);
+const conn = await db.connect();
+await conn.query('INSTALL httpfs;');
+await conn.query('LOAD httpfs;');
 
-  const res = db.exec("SELECT * FROM categories LIMIT 10");
-  console.log(res);
-})(this);
+await conn.query(`ATTACH '${window.location.origin}/photospa.db' AS photospa (READONLY)`);
+await conn.query(`USE photospa;`);
+
+window.duckdb = db;
+window.conn = conn;
+
+console.log('Ready!');
